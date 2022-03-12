c09126a4039751856da8f04eb465bce5f34e8842
han254 <peternjenga1706@gmail.com>
2022-03-12 17:37:03 +0300

    feat:form validation

--- JS/script.js
+++ JS/script.js
@@ -30,3 +30,21 @@ $(function () {
   });
 });
 
+function validateform() {
+  let name = document.myForm.inputName.value;
+  let email = document.myForm.inputEmail.value;
+  let message = document.myForm.inputMessage.value;
+
+  if (name == null || name == "") {
+    alert("Please fill in your name");
+    return false;
+  } else if (email == null || email == "") {
+    alert("Please enter your email!");
+    return false;
+  } else if (message == null || message == "") {
+    alert("Kindly write your message");
+    return false;
+  } else {
+    alert("Thank you " + name + " your message has been received");
+  }
+}
